5f3272fa22f7aba28b8e116394ead72178c7e86d
Franck <fkdiomande@icloud.com>
2026-06-12 23:13:46 +0200

    feat(components): Add barrel exports for button, error, loader, and not-found

---
+++ src/components/button/index.ts
@@ -0,0 +1,8 @@
+export { Button, buttonVariants } from "./Button";
+export { ButtonBack } from "./ButtonBack";
+export { ButtonCancel } from "./ButtonCancel";
+export { ButtonDelete } from "./ButtonDelete";
+export { ButtonEdit } from "./ButtonEdit";
+export { ButtonMore } from "./ButtonMore";
+export { ButtonNext } from "./ButtonNext";
+export { ButtonSave } from "./ButtonSave";
