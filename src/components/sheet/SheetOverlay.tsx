da10e022140f06f51309bdfe11d0e3bb7c0e2b77
Franck <fkdiomande@icloud.com>
2026-07-21 21:39:30 +0200

    style(common): Remove drawer/sheet side borders and darken overlay

--- src/components/sheet/SheetOverlay.tsx
+++ src/components/sheet/SheetOverlay.tsx
@@ -6,7 +6,7 @@ export const SheetOverlay = ({ className, ...props }: SheetPrimitive.Backdrop.Pr
     <SheetPrimitive.Backdrop
       data-slot="sheet-overlay"
       className={cn(
-        "data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 bg-black/10 duration-100 data-ending-style:opacity-0 data-starting-style:opacity-0 supports-backdrop-filter:backdrop-blur-xs fixed inset-0 z-50",
+        "data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 bg-black/30 duration-100 data-ending-style:opacity-0 data-starting-style:opacity-0 supports-backdrop-filter:backdrop-blur-xs fixed inset-0 z-50",
         className,
       )}
       {...props}
