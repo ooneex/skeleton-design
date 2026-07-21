e3babf76a9427b9efdd71ed8712859ce4f9f6e41
Franck <fkdiomande@icloud.com>
2026-07-21 10:13:26 +0200

    style(common): Use pointer and not-allowed cursors on command item

--- src/components/command/CommandItem.tsx
+++ src/components/command/CommandItem.tsx
@@ -9,7 +9,7 @@ export const CommandItem = ({ className, children, ...props }: ComponentProps<ty
     <CommandPrimitive.Item
       data-slot="command-item"
       className={cn(
-        "data-selected:bg-accent data-selected:text-accent-foreground data-selected:**:[svg]:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-lg px-2 py-2 text-sm outline-hidden select-none transition-colors duration-150 [&_svg:not([class*='size-'])]:size-4 group/command-item data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+        "data-selected:bg-accent data-selected:text-accent-foreground data-selected:**:[svg]:text-accent-foreground relative flex cursor-pointer items-center gap-2 rounded-lg px-2 py-2 text-sm outline-hidden select-none transition-colors duration-150 [&_svg:not([class*='size-'])]:size-4 group/command-item data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
         className,
       )}
       {...props}
